f233c152549480ceda1d611290e496371c06534d
Foniks <borisbeslimov@gmail.com>
2025-07-26 17:13:26 +0200

    style(Boris): Fixed indentation for chat.js, now uses 4 spaces tabs as everywhere else

--- Boris/js/contacts.js
+++ Boris/js/contacts.js
@@ -5,62 +5,62 @@
 */
 
 const Contacts = {
-   addContact(contact) {
-      Storage.addContact(contact);
-      Contacts.updateContactList();
-   },
+	addContact(contact) {
+		Storage.addContact(contact);
+		Contacts.updateContactList();
+	},
 
-   updateContactList(contactList = Storage.getAllContacts()) {
-      const contactListElement = document.getElementById("contacts-list");
-      contactListElement.innerHTML = ""; // Clear existing contacts
-      let contactsTree = [];
-      contactList.forEach(contact => {
-         const contactElement = document.createElement("div");
-         if (contact.basePath !== '/') {
-            const pathParts = contact.basePath.split('/');
-            let currentLevel = contactsTree;
-            pathParts.forEach(part => {
-               if (part) {
-                  let folder = currentLevel.find(f => f.name === part);
-                  if (!folder) {
-                     folder = { name: part, contacts: [] };
-                     currentLevel.push(folder);
-                  }
-                  currentLevel = folder.contacts;
-               }
-            });
-            currentLevel.push(contact);
-         } else {
-            contactsTree.push(contact);
-         }
-      });
-      Contacts.renderContacts(contactsTree);
-   },
+	updateContactList(contactList = Storage.getAllContacts()) {
+		const contactListElement = document.getElementById("contacts-list");
+		contactListElement.innerHTML = ""; // Clear existing contacts
+		let contactsTree = [];
+		contactList.forEach(contact => {
+			const contactElement = document.createElement("div");
+			if (contact.basePath !== '/') {
+				const pathParts = contact.basePath.split('/');
+				let currentLevel = contactsTree;
+				pathParts.forEach(part => {
+				if (part) {
+					let folder = currentLevel.find(f => f.name === part);
+					if (!folder) {
+						folder = { name: part, contacts: [] };
+						currentLevel.push(folder);
+					}
+					currentLevel = folder.contacts;
+				}
+				});
+				currentLevel.push(contact);
+			} else {
+				contactsTree.push(contact);
+			}
+		});
+		Contacts.renderContacts(contactsTree);
+	},
 
-   renderContacts(contactsTree, parentElement = document.getElementById("contacts-list")) {
-      contactsTree.forEach(contact => {
-         if (contact.contacts && contact.contacts.length > 0) {
-            const folder = contact;
-            const folderElement = document.createElement("div");
-            folderElement.classList.add("contact-folder");
-            folderElement.innerHTML = `<p>${folder.name}</p>`;
-            const subList = document.createElement("ul");
-            subList.classList.add("foldered-contacts");
-            Contacts.renderContacts(contact.contacts, subList);
-            folderElement.appendChild(subList);
-            parentElement.appendChild(folderElement);
-         } else {
-            const contactElement = document.createElement("li");
-            contactElement.classList.add("contact", "hover-effect");
-            contactElement.textContent = contact.name;
-            contactElement.dataset.id = contact.id;
-            parentElement.appendChild(contactElement);
-         }
-      });
-   },
+	renderContacts(contactsTree, parentElement = document.getElementById("contacts-list")) {
+		contactsTree.forEach(contact => {
+			if (contact.contacts && contact.contacts.length > 0) {
+				const folder = contact;
+				const folderElement = document.createElement("div");
+				folderElement.classList.add("contact-folder");
+				folderElement.innerHTML = `<p>${folder.name}</p>`;
+				const subList = document.createElement("ul");
+				subList.classList.add("foldered-contacts");
+				Contacts.renderContacts(contact.contacts, subList);
+				folderElement.appendChild(subList);
+				parentElement.appendChild(folderElement);
+			} else {
+				const contactElement = document.createElement("li");
+				contactElement.classList.add("contact", "hover-effect");
+				contactElement.textContent = contact.name;
+				contactElement.dataset.id = contact.id;
+				parentElement.appendChild(contactElement);
+			}
+		});
+	},
 
-   filterContacts(query) {
-      const contacts = Storage.filterContactsByName(query);
-      Contacts.updateContactList(contacts);
-   }
+	filterContacts(query) {
+		const contacts = Storage.filterContactsByName(query);
+		Contacts.updateContactList(contacts);
+	}
 }
